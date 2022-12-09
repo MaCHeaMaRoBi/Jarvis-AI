48986caa9989a0a3eebf78cfd2294c5851a3c9e6
MaCHeaMaRoBi <robi46805@gmail.com>
2022-12-09 16:39:30 +0200

    added weather command

--- NormalJarvis/main.py
+++ NormalJarvis/main.py
@@ -1,12 +1,15 @@
 import sys
 import pyttsx3
 import speech_recognition as sr
-import nltk
+# import nltk
 import datetime
 from AppOpener import run
 from neuralintents import GenericAssistant
 
-nltk.download('omw-1.4')
+from bs4 import BeautifulSoup
+import requests
+
+# nltk.download('omw-1.4')
 
 r = sr.Recognizer()
 engine = pyttsx3.init()
@@ -121,6 +124,25 @@ def show_username():
         talk(f"Your username is {f.read()}")
 
 
+def play_song():
+    talk("I cannot")
+
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+def weather():
+    city = "mioveni"
+    city = city + " weather"
+    city = city.replace(" ", "+")
+    res = requests.get(
+        f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',
+        headers=headers)
+    print("Searching...\n")
+    soup = BeautifulSoup(res.text, 'html.parser')
+    info = soup.select('#wob_dc')[0].getText().strip()
+    m_weather = soup.select('#wob_tm')[0].getText().strip()
+    talk(info + ", " + m_weather + "°C")
+
+
 mappings = {
     'time': time_now,
     'train': train_now,
@@ -128,7 +150,9 @@ mappings = {
     'open_app': open_app,
     'repeat': repeat,
     'exit': exit_jarvis,
-    'show_username': show_username
+    'show_username': show_username,
+    'play_song': play_song,
+    'weather': weather
 }
 
 assistant = GenericAssistant(
@@ -163,8 +187,6 @@ def username():
 
 
 def run_jarvis():
-    wish_me()
-    username()
     while True:
         message = get_command()
         if message is not None:
@@ -174,4 +196,6 @@ def run_jarvis():
 
 
 if __name__ == "__main__":
+    wish_me()
+    username()
     run_jarvis()
